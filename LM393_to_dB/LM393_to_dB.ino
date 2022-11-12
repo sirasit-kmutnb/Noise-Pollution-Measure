65bc7438bd65038ac67144648e3da4e12aeb3164
nonthawat-kmutnb <s6401012610018@email.kmutnb.ac.th>
2022-11-12 23:42:42 +0700

    Update LM393_to_dB.ino

--- LM393_to_dB/LM393_to_dB.ino
+++ LM393_to_dB/LM393_to_dB.ino
@@ -1,42 +1,36 @@
-
 #define SENSOR_PIN A0
-const int sampleWindow = 50; 
+const int sampleWindow = 50;
 unsigned int sample;
 void setup() {
 
   Serial.begin(9600);
-  pinMode (SENSOR_PIN, INPUT);
-  
+  pinMode(SENSOR_PIN, INPUT);
 }
 
-void loop() { 
-   unsigned long startMillis= millis();                   // Start of sample window
-   float peakToPeak = 0;                                  // peak-to-peak level
- 
-   unsigned int signalMax = 0;                            //minimum value
-   unsigned int signalMin = 1024;                         //maximum value
- 
-                                                          // collect data for 50 mS
-   while (millis() - startMillis < sampleWindow)
-   {
-      sample = analogRead(SENSOR_PIN);                    //get reading from microphone
-      if (sample < 1024)                                  // toss out spurious readings
-      {
-         if (sample > signalMax)
-         {
-            signalMax = sample;                           // save just the max levels
-         }
-         else if (sample < signalMin)
-         {
-            signalMin = sample;                           // save just the min levels
-         }
+void loop() {
+  unsigned long startMillis = millis();  // Start of sample window
+  float peakToPeak = 0;                  // peak-to-peak level
+
+  unsigned int signalMax = 0;     //minimum value
+  unsigned int signalMin = 1024;  //maximum value
+
+  // collect data for 50 mS
+  while (millis() - startMillis < sampleWindow) {
+    sample = analogRead(SENSOR_PIN);  //get reading from microphone
+    if (sample < 1024)                // toss out spurious readings
+    {
+      if (sample > signalMax) {
+        signalMax = sample;  // save just the max levels
+      } else if (sample < signalMin) {
+        signalMin = sample;  // save just the min levels
       }
-   }
- 
-   peakToPeak = signalMax - signalMin;                    // max - min = peak-peak amplitude
-  //  float db = map(peakToPeak,20,900,49.5,90);             //calibrate for deciBels   
-  int db = (peakToPeak+230.66265060241)/5.77835479850436 ; //calibrate for dB using linear regression (beta)
+    }
+  }
+
+  peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
+  //  float db = map(peakToPeak,20,900,49.5,90);             //calibrate for deciBels
+  int db = (peakToPeak + 230.66265060241) / 5.77835479850436;  //calibrate for dB using linear regression (beta)
   Serial.print(db);
   Serial.print(" ");
-  Serial.println(peakToPeak);   
+  Serial.println(peakToPeak);
 }
